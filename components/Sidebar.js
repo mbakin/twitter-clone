dc65cbad2eda783a62931b03f240f4c3b3d261f1
mbakin <mehmetberkakinn@outlook.com>
2021-12-16 20:52:37 +0300

    fix: background color black
    <main className='max-w-[1500px]'> it's not enough for background size

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,8 +5,8 @@ function Sidebar() {
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
       <div className="flex items-center justify-center w-14 h-14 p-0 xl:ml-24">
         <Image src="https://rb.gy/ogau5a" width={30} height={30} />
-
       </div>
+
     </div>
   )
 }
