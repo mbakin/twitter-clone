d2656ea93ef98bed61c546f979f7fe21e98585c9
mbakin <mehmetberkakinn@outlook.com>
2021-12-17 19:57:52 +0300

    create `Feed.js` component

---
+++ components/Feed.js
@@ -0,0 +1,9 @@
+function Feed() {
+  return (
+    <div className="text-white">
+      Feed page
+    </div>
+  )
+}
+
+export default Feed
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
       
       <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
         <Sidebar />
-        {/* feed */}
+        <Feed />
         {/* Widgets */}
         {/* Modal */}
 
