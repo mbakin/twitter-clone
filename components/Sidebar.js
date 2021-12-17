5c4234e89389e62c4d4707f65d706211766a27b1
mbakin <mehmetberkakinn@outlook.com>
2021-12-17 14:19:37 +0300

    install: @heroicons/react package and import in component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,17 @@
-import Image from "next/image"
-import SidebarLink from "./SidebarLink"
+import Image from "next/image";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/react/outline";
+
+import SidebarLink from "./SidebarLink";
 
 function Sidebar() {
   return (
@@ -8,10 +20,17 @@ function Sidebar() {
         <Image src="https://rb.gy/ogau5a" width={30} height={30} />
       </div>
       <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
-      <SidebarLink />
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Messages" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
