c35be8170607e3a650df4c2d4816ea07094f244e
mbakin <mehmetberkakinn@outlook.com>
2021-12-17 19:36:19 +0300

    add styles for `Tweet` button and user icon button in bottom

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -29,6 +29,15 @@ function Sidebar() {
         <SidebarLink text="Profile" Icon={UserIcon} />
         <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
       </div>
+      <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">Tweet</button>
+      <div className="text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto  mt-auto">
+        <img src="https://pbs.twimg.com/profile_images/1252531684353998848/6R0-p1Vf_400x400.jpg" className="h-10 w-10 rounded-full xl:mr-2.5" alt="user-icon" />
+        <div className="hidden xl:inline leading-5">
+          <h4 className="font-bold">Test user</h4>
+          <p className="text-[#6e767d]">@testuser</p>
+        </div>
+        <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
+      </div>
     </div>
   );
 }
