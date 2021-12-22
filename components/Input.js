fc92e8d3dfa1a69e28fd65612a70f3490cfa4e12
mbakin <mehmetberkakinn@outlook.com>
2021-12-22 17:15:44 +0300

    create: write functionality and style for input area

--- components/Input.js
+++ components/Input.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 function Input() {
+  const [input, setInput] = useState("");
+
   return (
-    <div className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}>
-      <img src="https://pbs.twimg.com/profile_images/1252531684353998848/6R0-p1Vf_400x400.jpg" className="h-11 w-11 rounded-full cursor-pointer" alt="user-icon" />
+    <div
+      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}
+    >
+      <img
+        src="https://pbs.twimg.com/profile_images/1252531684353998848/6R0-p1Vf_400x400.jpg"
+        alt="user-icon" className="h-11 w-11 rounded-full cursor-pointer"
+      />
       <div className="w-full divide-y divide-gray-700">
-        <div >
-          <textarea name="" id="" cols="30" rows="10"></textarea>
+        <div>
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            rows="2"
+            placeholder="What's happening?"
+            className="bg-transparent outline-none text-lg text-[#d9d9d9] placeholder-gray-500 w-full tracking-wide"
+          ></textarea>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Input
+export default Input;
