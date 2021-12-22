61687afd37950f392beff596019d4dbd79c17346
mbakin <mehmetberkakinn@outlook.com>
2021-12-22 17:38:45 +0300

    create `X` button functionality
    The purpose of this button is to cancel a selected photo.

--- components/Input.js
+++ components/Input.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { XIcon } from "@heroicons/react/outline";
 
 function Input() {
   const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
 
   return (
     <div
@@ -9,7 +11,8 @@ function Input() {
     >
       <img
         src="https://pbs.twimg.com/profile_images/1252531684353998848/6R0-p1Vf_400x400.jpg"
-        alt="user-icon" className="h-11 w-11 rounded-full cursor-pointer"
+        alt="user-icon"
+        className="h-11 w-11 rounded-full cursor-pointer"
       />
       <div className="w-full divide-y divide-gray-700">
         <div>
@@ -18,8 +21,20 @@ function Input() {
             onChange={(e) => setInput(e.target.value)}
             rows="2"
             placeholder="What's happening?"
-            className="bg-transparent outline-none text-lg text-[#d9d9d9] placeholder-gray-500 w-full tracking-wide"
-          ></textarea>
+            className="bg-transparent outline-none text-lg text-[#d9d9d9] placeholder-gray-500 w-full tracking-wide min-h-[50px]"
+          />
+
+          {selectedFile && (
+            <div className="relative">
+              <div className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer" onClick={() => setSelected(null)} >
+                <XIcon className="text-white h-5" />
+              </div>
+              <img
+                src={selectedFile}
+                className="rounded-2xl max-h-80 object-contain"
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
