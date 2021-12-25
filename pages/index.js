23e93422454e5dfda198e754df57adc0b323c133
mbakin <mehmetberkakinn@outlook.com>
2021-12-25 19:38:25 +0300

    create `getServerSideProps` function for dynamic information
    Next.js provides a get static props and server side props for information :100:

--- pages/index.js
+++ pages/index.js
@@ -21,3 +21,23 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
+}
